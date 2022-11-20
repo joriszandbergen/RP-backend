f5a688cb256b742d14290b6fe9fbf15babcc5e3f
joriszandbergen <62294353+joriszandbergen@users.noreply.github.com>
2022-11-20 17:02:39 +0100

    first commit!:) (a bit to late)

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  "https://www.google.com",
   "http;//127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
+  "https://rp-frontend.onrender.com",
+  "https://www.rp-frontend.onrender.com",
 ];
 
 module.exports = allowedOrigins;
