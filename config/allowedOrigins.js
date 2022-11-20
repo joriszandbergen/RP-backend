73f56f666884c8f7e8549ed80cf943f9f09e4f9f
joriszandbergen <62294353+joriszandbergen@users.noreply.github.com>
2022-11-20 20:42:07 +0100

    added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,4 @@
 const allowedOrigins = [
-  "http;//127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3000",
   "https://rp-frontend.onrender.com",
   "https://www.rp-frontend.onrender.com",
 ];
